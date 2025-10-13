5ed4ccedd9c7c8375035af284a60b7a0f2091b6e
Caroline Gustafsson <caroline.gustafsson@isof.se>
2025-10-13 11:44:37 +0000

    add code comments

--- download.js
+++ download.js
@@ -12,7 +12,9 @@
 let downloadableDictionaries;
 let downloadableDictionariesDict;
 
+/* --------- OPEN DOWNLOAD POPUP --------- */
 async function openDownload() {
+	// display download popup
     var h = document.getElementById("downloadPopup");
     h.style.display = "block";
 
@@ -23,35 +25,43 @@ async function openDownload() {
     $(".testVersionInfo").remove();
     $(h).prepend(testVersionDiv);*/
 
+	// get dictionaries
     downloadableDictionaries = await $.ajax({url:"lexin-downloadable.json", datatype: "json", type:"GET"});
     downloadableDictionariesDict = _.object(_.map(downloadableDictionaries, e => [e.lang, e]));
-    let dbServerValue = await dbServer.promise;
 
+	// get downloaded dictionaries
+    let dbServerValue = await dbServer.promise;
     let metadata = await dbServerValue.metadata.query().all().execute();
     let downloadedLanguages = _.map(metadata, (e) => e.version ? e.lang : null);
 
+	// default language option is empty
     $("#selectDownloadLanguages").empty();
     let e = $("<option></option>");
     e.text("");
     e.val("");
     $("#selectDownloadLanguages").append(e);
     e.prop("selected", true);
-    $("#languageChoice option").each(function () {
-	let lang = $(this).val();
-	let e = $("<option></option>");
-	e.text($(this).text());
-	e.val(lang);
-	$("#selectDownloadLanguages").append(e);
 
-	if (_.contains(downloadedLanguages, $(this).val())) {
-	    e.prop("disabled", true);
-	} else {
-	    e.prop("selected", this.selected);
-	}
+	// add languages to selector dropdown
+    $("#languageChoice option").each(function () {
+		let lang = $(this).val();
+		let e = $("<option></option>");
+		e.text($(this).text());
+		e.val(lang);
+		$("#selectDownloadLanguages").append(e);
+
+		// disable already downloadeds dictionaries
+		if (_.contains(downloadedLanguages, $(this).val())) {
+			e.prop("disabled", true);
+		} else {
+			e.prop("selected", this.selected);
+		}
     });
+
+	// update UI
     $("#selectDownloadLanguages").off("change");
     $("#selectDownloadLanguages").on("change", function () {
-	redrawDownload();
+		redrawDownload();
     });
     redrawDownload();
 
@@ -60,60 +70,80 @@ async function openDownload() {
 	startDownload();
     });*/
 
-	// CG ADD - THIS POSSIBLY FIXES THE "DUPLICATE DOWNLOADS" BUG
+	// CG ADD - THIS FIXES THE "DUPLICATE DOWNLOADS" BUG
 	$(".downloadStartButton")
-	.off("click.lexin")                // namespaced off
+	.off("click.lexin")
 	.on("click.lexin", function (e) {
 		e.preventDefault();
 		startDownload();
 	});
 }
 
+/* --------- DOWNLOAD CHOSEN DICTIONARIES --------- */
 function startDownload() {
     _.each(downloadLanguages, language => {
-	console.log("download", language);
-	(async() => {
-	    try {
-		await doDownload(language.progressWrapper, language.lang);
-	    } catch {
-		language.progressWrapper.empty().append("Nedladdning misslyckades");
-	    }
-	})();
+		console.log("download", language);
+		(async() => {
+			try {
+				await doDownload(language.progressWrapper, language.lang);
+			} catch {
+				language.progressWrapper.empty().append("Nedladdning misslyckades");
+			}
+		})();
     });
 }
 
+/* --------- AFTER DICTIONARY DOWNLOAD --------- */
 function setAsDownloaded(wrapper, lang) {
+	// show info text
     wrapper.empty().append("Nedladdat <button class='removeDownloadButton' type='button'></button>");
+
+	// remove button event
     wrapper.find("button").click(async function () {
-	let progress = $("<progress></progress>");
-	wrapper.empty().append(progress).val(0);
-	await removeLanguage(lang, progress);
-	let dbServerValue = await dbServer.promise;
-	let metadataEntries = await dbServerValue.metadata.query("lang").only(lang).execute()
-	await Promise.all(metadataEntries.map(e => {
-	    console.log(Date.now(), "removebutton metadata remove id", e.id);
-	    return dbServerValue.metadata.remove(e.id)
-	}))
-	wrapper.empty()
-//	redrawDownload();
+		// show progress bar
+		let progress = $("<progress></progress>");
+		wrapper.empty().append(progress).val(0);
+		await removeLanguage(lang, progress);
+
+		// remove dictionary metadata
+		let dbServerValue = await dbServer.promise;
+		let metadataEntries = await dbServerValue.metadata.query("lang").only(lang).execute()
+		await Promise.all(metadataEntries.map(e => {
+			console.log(Date.now(), "removebutton metadata remove id", e.id);
+			return dbServerValue.metadata.remove(e.id)
+		}))
+
+		// update UI 
+		wrapper.empty()
+		//redrawDownload();
+
+		// CG ADD
+		//openDownload();
     });
+
+	// CG add
+	//redrawDownload();
 }
 
+/* --------- HANDLE DOWNLOAD FAIL --------- */
 function activateRedownloadButton(wrapper, lang) {
     wrapper.find("button").click(async function () {
-	try {
-	    await doDownload(wrapper, lang);
-	} catch(e) {
-	    console.log(e.stack);
-	    console.log(e.name);
-	    console.log(e.message);
-	    wrapper.empty().append("Nedladdning misslyckades");
-	}
+		try {
+			await doDownload(wrapper, lang);
+		} catch(e) {
+			console.log(e.stack);
+			console.log(e.name);
+			console.log(e.message);
+			wrapper.empty().append("Nedladdning misslyckades");
+		}
     });
 }
 
+/* --------- REMOVE DICTIONARY --------- */
 async function removeLanguage(lang, progress, wantedStatus) {
     console.log(Date.now(), "removeLanguage", lang);
+
+	// get language metadata
     let dbServerValue = await dbServer.promise;
     let metadataEntries = await dbServerValue.metadata.query("lang").only(lang).execute()
     if (!wantedStatus) {
@@ -121,6 +151,7 @@ async function removeLanguage(lang, progress, wantedStatus) {
     }
     await dbServerValue.metadata.query("lang").only(lang).modify({status:wantedStatus}).execute()
 
+	// remove all the language's entries
     let ids = await dbServerValue.entries.query("lang").only(lang).execute()
     console.log(Date.now(), "removeLanguage", ids.length);
     let chunks = _.chunk(ids, 100);
@@ -128,80 +159,79 @@ async function removeLanguage(lang, progress, wantedStatus) {
     for (const chunk of chunks) {
 		let removePromises = _.map(chunk, (e, i) => {
 			return dbServerValue.entries.remove(e.id)
-	})
-	await Promise.all(removePromises)
-	i++;
-	if (progress) {
-	    progress.val(i/chunks.length);
-	}
+		})
+		await Promise.all(removePromises)
+		i++;
+		if (progress) {
+			progress.val(i/chunks.length);
+		}
     }
-    console.log(Date.now(), "removeLanguage removed all entries");
+
     console.log(Date.now(), "removeLanguage complete");
-
-	// CG ADD
-	// 1) Remove metadata row for this lang so UI logic won’t treat it as downloaded
-	//await dbServerValue.metadata.remove(lang);
-
-	// 2) Rebuild the whole download section (dropdown + list)
-	//await redrawDownload();
 }
 
+/* --------- DOWNLOAD DICTIONARY --------- */
 async function doDownload(wrapper, lang) {
     let metadata = downloadableDictionariesDict[lang];
+
+	// show progress bar
     let progress = $("<progress></progress>");
     wrapper.empty().append(progress).val(0);
-    let dbServerValue = await dbServer.promise;
 
+	// get language metadata
+    let dbServerValue = await dbServer.promise;
     await dbServerValue.metadata.add({"lang": lang, "version": metadata.version, "status": "adding"});
     console.log("doDownload", await dbServerValue.metadata.query().all().execute());
     
+	// get file
     let filename = downloadableDictionariesDict[lang].filename;
-
     console.log(Date.now(), "start fetching file");
-    const json_dict = await $.ajax({
-	xhr: function() {
-	    let xhr = new window.XMLHttpRequest();
-	    xhr.onprogress = (e) => {
-//		console.log("progress", e);
-		progress.val(e.loaded / metadata.size / 2);
-	    };
-	    return xhr;
-	},
-	url:filename,
-	datatype: "json",
-	type:"GET"
+	const json_dict = await $.ajax({
+		xhr: function() {
+			let xhr = new window.XMLHttpRequest();
+			xhr.onprogress = (e) => {
+				// console.log("progress", e);
+				progress.val(e.loaded / metadata.size / 2);
+			};
+			return xhr;
+		},
+		url:filename,
+		datatype: "json",
+		type:"GET"
     });
+	
     const entries_dict = json_dict.entries;
     await dbServerValue.metadata.query("lang").only(lang).modify({wordbase:json_dict.WordBase}).execute();
     console.log(Date.now(), "fetched file");
+
     await removeLanguage(lang, wrapper, "adding");
     let keys = Object.keys(entries_dict);
     for (const id of keys) {
-	//	    console.log("entry", id);
-	entries_dict[id].lang = lang;
-	entries_dict[id].index = [];
-	for (const type_index of entries_dict[id].indices) {
-	    entries_dict[id].index.push(type_index[1])
-	}
-	delete entries_dict[id].indices;
+		// console.log("entry", id);
+		entries_dict[id].lang = lang;
+		entries_dict[id].index = [];
+		for (const type_index of entries_dict[id].indices) {
+			entries_dict[id].index.push(type_index[1])
+		}
+		delete entries_dict[id].indices;
     }
     console.log(Date.now(), "extended entries");
     let chunks = _.chunk(Object.values(entries_dict), 100);
     let i = 0;
     try {
-	for (const chunk of chunks) {
-	    await dbServerValue.entries.add(chunk);
-	    i++;
-	    progress.val(i / chunks.length / 2 + 0.5);
-	}
-	console.log(Date.now(), "stored entries");
-	await dbServerValue.metadata.query("lang").only(lang).modify({status:"active"}).execute();
-	console.log("doDownload", await dbServerValue.metadata.query().all().execute());
-	setAsDownloaded(wrapper, lang);
-//	redrawDownload();
+		for (const chunk of chunks) {
+			await dbServerValue.entries.add(chunk);
+			i++;
+			progress.val(i / chunks.length / 2 + 0.5);
+		}
+		console.log(Date.now(), "stored entries");
+		await dbServerValue.metadata.query("lang").only(lang).modify({status:"active"}).execute();
+		console.log("doDownload", await dbServerValue.metadata.query().all().execute());
+		setAsDownloaded(wrapper, lang);
+		// redrawDownload();
     } catch {
-	console.log("add failed for lang", lang, "chunk", i);
-//	redrawDownload();
+		console.log("add failed for lang", lang, "chunk", i);
+		// redrawDownload();
     }
 }
 
